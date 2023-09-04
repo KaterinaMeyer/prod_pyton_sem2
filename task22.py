606e41064bd59a1338e2d1a0efc29e2c3632c262
KaterinaMeyer <katttum@yandex.ru>
2023-09-04 18:26:03 +0300

    задача 1

---
+++ task22.py
@@ -0,0 +1,18 @@
+n = int(input("Введите количество элементов первого множества: "))
+m = int(input("Введите количество элементов второго множества: "))
+
+set1 = set()
+set2 = set()
+
+print("Введите элементы первого множества:")
+for i in range(n):
+    element = int(input())
+    set1.add(element)
+
+print("Введите элементы второго множества:")
+for i in range(m):
+    element = int(input())
+    set2.add(element)
+
+common_elements = sorted(set1.intersection(set2))
+print( "ответ" common_elements)
